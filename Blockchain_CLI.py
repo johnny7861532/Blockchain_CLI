ca98655e849cd39d65549dbc8d481b66fae208ea
JohnnyHsieh <johnny7861532@hotmail.com>
2018-12-06 15:13:55 +0800

    Update Blockchain_CLI.py
    Add ICON deployed & get txresult & get contract api CLI commend.

--- Blockchain_CLI.py
+++ Blockchain_CLI.py
@@ -9,7 +9,6 @@ Created on Wed Dec  5 10:33:18 2018
 
 import click
 import subprocess
-from time import sleep
 
 
 
@@ -22,19 +21,16 @@ __author__ = "Johnny Hsieh"
 def Blockchain():
     """                                                                        
     Very simple CLI to start a different protocol from local.
-    Supporting NEO
+    Supporting NEO and ICON.
     """
     pass
 
 
 
 
-@Blockchain.command()
+@Blockchain.command(help="Choose the network you want to conncet.")
 @click.option('--networkid', type=click.Choice(['testnet', 'mainnet']),prompt=True)  
 def StartNEO(networkid):
-    """
-    Choose the network you want to conncet.
-    """
     if networkid == "testnet":
         initTestnet()
     else:
@@ -57,11 +53,9 @@ def initMainnet():
     
  
 
-@Blockchain.command()
+@Blockchain.command(help="Install the NEO kit inorder to start NEO node.")
 def installNEO():
-    """
-    Install the NEO kit inorder to start NEO node.
-    """
+
     click.confirm('This commend will install neo-python, Do you want to continue?', abort=True)
     subprocess.call(["pip3","install","neo-python"]) 
     subprocess.call(["git","clone","https://github.com/CityOfZion/neo-python.git"])
@@ -69,12 +63,11 @@ def installNEO():
     click.echo(click.style('Envirment is ready! You can start a NEO node by typing ',bg='black', fg='white'))
     click.echo(click.style('python3 Blockchain_CLI.py startneo',bg='white', fg='red'))
     
-@Blockchain.command()
+@Blockchain.command(help="Install the ICON kit inorder to start ICON node.")
 def installICON():
-    """
-    Install the ICON kit inorder to start ICON node.
-    """
+   
     click.confirm('This commend will install mutilple libraries, Do you want to continue?', abort=True)
+    
     subprocess.call(["brew","install","leveldb"])
     subprocess.call(["brew","install","rabbitmq"]) 
     subprocess.call(["brew","install","automake"]) 
@@ -87,7 +80,7 @@ def installICON():
     click.echo(click.style('Envirment is ready! You can start a ICON node by typing ',bg='black', fg='white'))
     click.echo(click.style('python3 Blockchain_CLI.py starticon',bg='white', fg='red'))
     
-@Blockchain.command()
+@Blockchain.command(help="Start ICON node and begin to interact with tbears.")
 def startICON():
     click.echo(click.style('ICON', fg='green', blink=True, bold=True))
     click.echo(click.style('Node is init!',bg='black', fg='white'))
@@ -98,12 +91,42 @@ def startICON():
     subprocess.call(["tbears", "lastblock"]) 
     
     
-@Blockchain.command()
+@Blockchain.command(help="Stop ICON node and close rabbitmq and tbears.")
 def stopICON():
     subprocess.call(["brew", "services", "stop" ,"rabbitmq"]) 
     subprocess.call(["tbears","stop"])
     click.echo(click.style('ICON', fg='green', blink=True, bold=True))
     click.echo(click.style('Node is kill',bg='black', fg='white'))
+    
+@Blockchain.command(help="Using ICON template ABCToken to test simple contract.")
+def testICONCOntract():
+    click.echo(click.style('ICON', fg='green', blink=True, bold=True))
+    click.echo(click.style('contract depolying....',bg='black', fg='white'))  
+    subprocess.call(["tbears","init", "abc", "ABCToken"])
+    subprocess.call(["tbears","ls","abc"])
+    subprocess.call(["tbears", "deploy", "-t","tbears","abc"])
+    click.echo(click.style('Please using this command to get the result:',bg='black', fg='white'))
+    click.echo(click.style('python3 Blockchain_CLI.py geticontx tx',bg='black', fg='red'))
+    click.echo(click.style('Please using this command to get the result:',bg='black', fg='white'))
+    click.echo(click.style('python3 Blockchain_CLI.py geticoncontractapi tx',bg='black', fg='red'))
+    
+@Blockchain.command(help="Get the result from txhash")
+@click.argument('tx')
+def getICONtx(tx):
+    click.echo(click.style('ICON', fg='green', blink=True, bold=True))
+    click.echo(click.style('get tx result:',bg='black', fg='white'))  
+    subprocess.call(["tbears","txresult", tx])
+    
+    
+@Blockchain.command(help="Get the contract api")
+@click.argument('tx')  
+def getICONContractapi(tx):
+    click.echo(click.style('ICON', fg='green', blink=True, bold=True))
+    click.echo(click.style('get contract api:',bg='black', fg='white'))  
+    subprocess.call(["tbears","scoreapi",tx])
+    
+
+    
     
 
 if __name__ == '__main__':
